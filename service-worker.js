e52c2d62c3eff96c60abdcdc257b3cb5213bded9
Marcos Katz <marcoskatz@grupoingeco.com.ar>
2026-07-27 16:03:27 -0300

    Bump SW cache to v2 (index.html changed in the review-fix pass)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'trituracion-v1';
+const CACHE = 'trituracion-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
