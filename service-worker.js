499643d5233fd586a76a3612e633dcf8465cc759
Marcos Katz <marcoskatz@grupoingeco.com.ar>
2026-07-27 17:49:07 -0300

    Agregar título 'Producido hoy' a la pestaña Producido

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'trituracion-v5';
+const CACHE = 'trituracion-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo-ingeco.png'];
 
 self.addEventListener('install', (event) => {
